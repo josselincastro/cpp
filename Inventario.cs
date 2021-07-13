05e3f5b63cebb423be47f35cb1e0850cf571c347
josselincastro <84490514+josselincastro@users.noreply.github.com>
2021-07-12 22:35:51 -0600

    Update Inventario.cs

--- Inventario.cs
+++ Inventario.cs
@@ -1,81 +1,35 @@
 using System;
 using System.Collections.Generic;
 
-
 public class Inventario
 {
+    public List<Producto> ListadeProductos { get; set; }
+    public Inventario()
+    {
+        ListadeProductos = new List<Producto>();
 
-public List< Producto> Listadeproductos {get; set; }
-}
-public Inventario ()
-{
-ListadeProductos = new List<Producto> ();
-Producto a = new Product ("001" , "iPhoneX" , 0);
-Producto b = new Product ("002" , "Laptop Dell" , 5);
-Producto c = new Product ("003" , "Monitor samsung" , 2);
-Producto d = new Product ("004" , "Mouse" , 100);
-Producto e = new Product ("005" , "Headset" , 25);
+        Producto a = new Producto("001", "iPhoneX", 0);
+        Producto b = new Producto("002", "Dell", 5);
+        Producto c = new Producto("003", "Monitor Samsung", 2);
+        Producto d = new Producto("004", "Mouse", 100);
+        Producto e = new Producto("005", "Headset", 25);
 
-ListadeProductos.Add(a)
-ListadeProductos.Add(b)
-ListadeProductos.Add(c)
-ListadeProductos.Add(d)
-ListadeProductos.Add(e)
-}
-private void listarProductos(){
-    console.Clear ();
-    console.WriteLine("");
-     console.WriteLine("Listado de Productos");
-     console.WriteLine("********************");
-     console.WriteLine("Codigo, Descripción y Existencia");
+        ListadeProductos.Add(a);
+        ListadeProductos.Add(b);
+        ListadeProductos.Add(c);
+        ListadeProductos.Add(d);
+        ListadeProductos.Add(e);
+    }
 
     
-    foreach (var product in ListadeProductos)
-    {
-       console.WriteLine(productos.codigo + " | " + producto.Descripcion + " | " + producto.Existencia.ToString()); 
-    }
-    Console.ReadLine();
-}
+    public void listarProductos() {
+        Console.Clear();
+        Console.WriteLine("");
+        Console.WriteLine("Listado de Productos");
+        Console.WriteLine("********************");
+        Console.WriteLine("Codigo, Descripcion y Existencia");
 
-private void movimientoInventario(string codigo, int cantidad, string tipoMovimiento){
-   foreach (var producto in ListadodeProductos)
-    {
-    if (producto.Codigo == codigo){
-            if (tipoMovimiento == "+"){
-                productos.Existencia = producto.Existencia + cantidad;
-            } else {
-                productos.Existencia = producto.Existencia - cantidad
-                ;
-            }
-        }  
-    }
-}
-
-public void ingresoDeInventario() {
-    string codigo = "";
-   string cantidad = "";
-
-    console.Clear ();
-    console.WriteLine("");
-
-     console.WriteLine("Ingreso de Productos al Inventario");
-     console.WriteLine("**********************************");
-  console.WriteLine("Ingrese el codigo del producto");
-  codigo = Console.ReadLine();
-      console.WriteLine("Ingrese la cantidad del producto");
-       cantidad = Console.ReadLine();
-     
-     movimientoInventario(codigo, Int32.Parse(cantidad), "+");
-    }
-}
-private void listarProductos(){
-    console.Clear ();
-    console.WriteLine("");
-     console.WriteLine("Listado de Productos");
-     console.WriteLine("********************");
-     console.WriteLine("Codigo, Descripción y Existencia");
-}
-   foreach (var producto in ListadeProductos)
+        foreach (var producto in ListadeProductos)
         {
             Console.WriteLine(producto.Codigo + " | " + producto.Descripcion + " | " + producto.Existencia.ToString());
         }
@@ -112,6 +66,4 @@ private void listarProductos(){
 
         movimientoInventario(codigo, Int32.Parse(cantidad), "+");
     }
-} 
-
-    
+}
