1e25371489151078db2104ba90ffeaea31507a50
josselincastro <84490514+josselincastro@users.noreply.github.com>
2021-11-18 00:42:34 -0600

    Pizzeria 5

---
+++ BL.Pizzeria/Factura.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Pizzeria
+{
+    public class Factura
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente cliente { get; set; }
+        public BindingList<FacturaDetalle> FacturaDetalle { get; set; }
+        public double Subtotal { get; set; }
+        public double Impuesto { get; set; }
+        public double Total { get; set; }
+        public bool Activo { get; set; }
+
+        public Factura()
+        {
+            Fecha = DateTime.Now;
+            FacturaDetalle = new BindingList<FacturaDetalle>();
+            Activo = true;
+        }
+    }
+
+    public class FacturaDetalle
+    {
+        public int Id { get; set; }
+        public int ProductoId { get; set; }
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public int Precio { get; set; }
+        public int Subtotal { get; set; }
+
+        public FacturaDetalle()
+        {
+            Cantidad = 1;
+        }
+    }
+}
---
+++ BL.Pizzeria/contexto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Pizzeria
+{
+    public class contexto : DbContext
+    {
+        public contexto() : base(@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDBFilename=" +
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\MiBaseDeDatosL3_6pm.mdf")
+        { 
+                       
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            Database.SetInitializer(new DatosdeInicio());
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Factura> Facturas { get; set; }
+        public DbSet<FacturaDetalle> FacturaDetalle { get; set; }
+        public object Tipos { get; internal set; }
+    }
+}
